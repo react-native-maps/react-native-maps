3eb334542debe4f96b898ec3bfbd625dd77e7946
Christopher Dro <hello@blick-labs.com>
2016-03-01 14:10:55 -0800

    [Docs] Update Android installation to reflect new versions of RN. Closes #105

--- example/examples/DraggableMarkers.js
+++ example/examples/DraggableMarkers.js
@@ -49,16 +49,14 @@ var MarkerTypes = React.createClass({
         >
           <MapView.Marker
             coordinate={this.state.a}
-            onDragEnd={(e) => this.setState({ a: e.nativeEvent.coordinate })}
+            onSelect={(e) => console.log('onSelect', e)}
+            onDrag={(e) => console.log('onDrag', e)}
+            onDragStart={(e) => console.log('onDragStart', e)}
+            onPress={(e) => console.log('onPress', e)}
             draggable
           >
             <PriceMarker amount={99} />
           </MapView.Marker>
-          <MapView.Marker
-            coordinate={this.state.b}
-            onDragEnd={(e) => this.setState({ b: e.nativeEvent.coordinate })}
-            draggable
-          />
         </MapView>
       </View>
     );
