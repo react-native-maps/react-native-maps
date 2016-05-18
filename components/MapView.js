ae98237fb061fdbcd71be56c0468d17ece40d704
Juliano Castilho <julianocomg@gmail.com>
2016-05-18 16:27:32 -0400

    Updating innerPadding when the prop changes

--- components/MapView.js
+++ components/MapView.js
@@ -297,6 +297,10 @@ var MapView = React.createClass({
   },
 
   componentWillUpdate: function(nextProps) {
+    if (JSON.stringify(this.props.innerPadding) !== nextProps.innerPadding) {
+      this.refs.map.setNativeProps({innerPadding: nextProps.innerPadding})
+    }
+    
     var a = this.__lastRegion;
     var b = nextProps.region;
     if (!a || !b) return;
