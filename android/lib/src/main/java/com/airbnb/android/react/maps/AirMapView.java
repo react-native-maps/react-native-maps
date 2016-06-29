35c955bf7656c988057e0f12967833691f8a9f5c
Jean-Richard Lai <jrichardlai@gmail.com>
2016-06-28 21:33:02 -0700

    [FIXBUG] Not calling setLoadingIndicatorColor when null

--- android/lib/src/main/java/com/airbnb/android/react/maps/AirMapView.java
+++ android/lib/src/main/java/com/airbnb/android/react/maps/AirMapView.java
@@ -610,7 +610,9 @@ public class AirMapView extends MapView implements GoogleMap.InfoWindowAdapter,
             this.mapLoadingProgressBar = new ProgressBar(getContext());
             this.mapLoadingProgressBar.setIndeterminate(true);
         }
-        this.setLoadingIndicatorColor(this.loadingIndicatorColor);
+        if (this.loadingIndicatorColor != null) {
+            this.setLoadingIndicatorColor(this.loadingIndicatorColor);
+        }
         return this.mapLoadingProgressBar;
     }
 
