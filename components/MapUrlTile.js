89dd0c1faf1f41a0b944977fbda4dcc0cdc4b780
Spike Brehm <spike@airbnb.com>
2016-09-20 12:06:40 -0700

    Fix some changes missed in rebase

--- components/MapUrlTile.js
+++ components/MapUrlTile.js
@@ -1,44 +1,40 @@
-
-var React = require('react-native');
-var {
+import React, { PropTypes } from 'react';
+import {
   View,
-  NativeMethodsMixin,
   requireNativeComponent,
-  StyleSheet,
-  PropTypes,
-} = React;
+} from 'react-native';
 
-var MapUrlTile = React.createClass({
-  mixins: [NativeMethodsMixin],
+const propTypes = {
+  ...View.propTypes,
 
-  propTypes: {
-    ...View.propTypes,
+  /**
+   * The url template of the tile server. The patterns {x} {y} {z} will be replaced at runtime
+   * For example, http://c.tile.openstreetmap.org/{z}/{x}/{y}.png
+   */
+  urlTemplate: PropTypes.string.isRequired,
 
-    /**
-     * The url template of the tile server. The patterns {x} {y} {z} will be replaced at runtime
-     * For example, http://c.tile.openstreetmap.org/{z}/{x}/{y}.png
-     */
-    urlTemplate: PropTypes.string.isRequired,
+  /**
+   * The order in which this tile overlay is drawn with respect to other overlays. An overlay
+   * with a larger z-index is drawn over overlays with smaller z-indices. The order of overlays
+   * with the same z-index is arbitrary. The default zIndex is -1.
+   *
+   * @platform android
+   */
+  zIndex: PropTypes.number,
+};
 
-    /**
-     * The order in which this tile overlay is drawn with respect to other overlays. An overlay
-     * with a larger z-index is drawn over overlays with smaller z-indices. The order of overlays
-     * with the same z-index is arbitrary. The default zIndex is -1.
-     *
-     * @platform android
-     */
-    zIndex: PropTypes.number,
-  },
-
-  render: function() {
+class MapUrlTile extends React.Component {
+  render() {
     return (
       <AIRMapUrlTile
         {...this.props}
       />
     );
-  },
-});
+  }
+}
 
-var AIRMapUrlTile = requireNativeComponent('AIRMapUrlTile', MapUrlTile);
+MapUrlTile.propTypes = propTypes;
+
+const AIRMapUrlTile = requireNativeComponent('AIRMapUrlTile', MapUrlTile);
 
 module.exports = MapUrlTile;
