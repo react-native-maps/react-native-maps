1d9d5296a6c68a977fafd20b605512a63d488a39
IjzerenHein <hrutjes@gmail.com>
2016-09-28 23:03:28 +0200

    Added support for taking snapshots on Android
    # Conflicts:
    #	android/src/main/java/com/airbnb/android/react/maps/AirMapModule.java

--- components/MapView.js
+++ components/MapView.js
@@ -446,8 +446,39 @@ class MapView extends React.Component {
   }
 
   takeSnapshot(width, height, region, callback) {
-    const finalRegion = region || this.props.region || this.props.initialRegion;
-    this._runCommand('takeSnapshot', [width, height, finalRegion, callback]);
+    return new Promise((resolve, reject) => {
+      let options;
+      if (typeof width === 'object') {
+        options = width;
+      }
+      else {
+        options = {
+          width: width,
+          height: height,
+          region: region || this.props.region || this.props.initialRegion
+        };
+      }
+      if (Platform.OS === 'android') {
+        NativeModules.AirMapModule.takeSnapshot(this._getHandle(), options).then((snapshot) => {
+          if (callback) callback(undefined, {uri: snapshot});
+          resolve(snapshot);
+        }, (err) => {
+          if (callback) callback(err);
+          reject(err);
+        });
+      }
+      else {
+        this._runCommand('takeSnapshot', [options.width, options.height, options.region, (err, snapshot) => {
+          if (callback) callback(err, snapshot);
+          if (err) {
+            reject(err);
+          }
+          else {
+            resolve(snapshot.uri);
+          }
+        }]);
+      }
+    });
   }
 
   _uiManagerCommand(name) {
