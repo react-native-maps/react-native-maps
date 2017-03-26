b3b743547ed0f8a37045dc1bc3a6f773a9c4fe24
Leland Richardson <leland.m.richardson@gmail.com>
2017-03-26 15:53:40 -0700

    Restructure project

--- example/android/app/src/main/java/com/airbnb/android/react/maps/example/ExampleApplication.java
+++ example/android/app/src/main/java/com/airbnb/android/react/maps/example/ExampleApplication.java
@@ -7,14 +7,20 @@ import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
+import com.facebook.soloader.SoLoader;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class ExampleApplication extends Application implements ReactApplication {
   private final ReactNativeHost reactNativeHost = new ReactNativeHost(this) {
-    @Override protected boolean getUseDeveloperSupport() {
+    @Override public boolean getUseDeveloperSupport() {
       return BuildConfig.DEBUG;
+    }
+
+    @Override
+    protected String getJSMainModuleName() {
+      return "example/index";
     }
 
     @Override protected List<ReactPackage> getPackages() {
@@ -27,5 +33,11 @@ public class ExampleApplication extends Application implements ReactApplication 
   @Override
   public ReactNativeHost getReactNativeHost() {
     return reactNativeHost;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    SoLoader.init(this, /* native exopackage */ false);
   }
 }
