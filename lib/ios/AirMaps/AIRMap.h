4926f5b53fac8e1ab9aaf877e81048e262dee8aa
Janic Duplessis <janicduplessis@gmail.com>
2017-03-27 18:24:24 -0400

    Rename MapKit category to avoid conflicts with the one in RN

--- lib/ios/AirMaps/AIRMap.h
+++ lib/ios/AirMaps/AIRMap.h
@@ -12,7 +12,7 @@
 
 #import <React/RCTComponent.h>
 #import "SMCalloutView.h"
-#import "RCTConvert+MapKit.h"
+#import "RCTConvert+AirMap.h"
 
 extern const CLLocationDegrees AIRMapDefaultSpan;
 extern const NSTimeInterval AIRMapRegionChangeObserveInterval;
