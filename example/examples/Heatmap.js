e7cfabc5d1be1fa9203829b63d66b2aa2b040453
Matt Furden <zolrath@gmail.com>
2017-08-11 16:43:27 -0700

    Bend the knee to eslint

--- example/examples/Heatmap.js
+++ example/examples/Heatmap.js
@@ -1,89 +1,91 @@
-import React from 'react'
-import MapView from 'react-native-maps'
+import React from 'react';
+import MapView from 'react-native-maps';
 
 import {
   StyleSheet,
-  PropTypes,
   View,
   Text,
   Dimensions,
   TouchableOpacity,
-  Image,
-} from 'react-native'
+} from 'react-native';
 
-
-let { width, height } = Dimensions.get('window');
+const { width, height } = Dimensions.get('window');
 
 const ASPECT_RATIO = width / height;
 const LATITUDE = 37.78825;
 const LONGITUDE = -122.4324;
 const LATITUDE_DELTA = 0.0922;
 const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
-const SPACE = 0.01;
-
-let getHeatMapsPoints = (size, withWeight = false) => {
-
-  let points = [];
-
-  for (i = 0; i < size; i++) {
-    let pointData = {latitude: LATITUDE + Math.random() / 50, longitude: LONGITUDE + Math.random() / 50};
-    if (withWeight) {
-      pointData.weight = Math.round(Math.random() * 10 + 1);
-    }
-    points.push(pointData);
-  }
-
-  return points;
-}
 
 class Heatmap extends React.Component {
-
   constructor(props) {
     super(props);
 
     this.state = {
-      points: getHeatMapPoints(50),
+      points: this.getHeatMapPoints(50),
       weightEnabled: false,
-    }
+    };
   }
 
+  getHeatMapsPoints = (size, withWeight = false) => {
+    const points = [];
+
+    for (let i = 0; i < size; i++) {
+      const pointData = {
+        latitude: LATITUDE + (Math.random() / 50),
+        longitude: LONGITUDE + (Math.random() / 50),
+      };
+      if (withWeight) {
+        pointData.weight = Math.round((Math.random() * 10) + 1);
+      }
+      points.push(pointData);
+    }
+
+    return points;
+  };
 
   changeHeatmap = () => {
-    this.setState({points: getHeatMapPoints(50,this.state.weightEnabled)})
-  }
+    this.setState({
+      points: this.getHeatMapPoints(50, this.state.weightEnabled),
+    });
+  };
 
   toggleWeightEnabled = () => {
-    this.setState({weightEnabled: !this.state.weightEnabled})
-  }
+    this.setState({ weightEnabled: !this.state.weightEnabled });
+  };
 
-  render = () => {
-    return (
-      <View style={styles.container}>
-        <MapView
-        ref="map"
-        style = {styles.map}
-        initialRegion = {{
+  render = () => (
+    <View style={styles.container}>
+      <MapView
+        style={styles.map}
+        initialRegion={{
           latitude: LATITUDE,
           longitude: LONGITUDE,
           latitudeDelta: LATITUDE_DELTA,
           longitudeDelta: LONGITUDE_DELTA,
         }}
+      >
+        <MapView.Heatmap points={this.state.points} />
+      </MapView>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity
+          onPress={this.changeHeatmap}
+          style={[styles.bubble, styles.button]}
         >
-        <MapView.Heatmap points={this.state.points}/>
-        </MapView>
-        <View style={styles.buttonContainer}>
-          <TouchableOpacity onPress={this.changeHeatmap} style={[styles.bubble, styles.button]}>
-            <Text>Update</Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={this.toggleWeightEnabled} style={[styles.bubble, styles.button]}>
-            <Text>{this.state.weightEnabled ? "With weight" : 'Without weight'}</Text>
-          </TouchableOpacity>
-        </View>
+          <Text>Update</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={this.toggleWeightEnabled}
+          style={[styles.bubble, styles.button]}
+        >
+          <Text>
+            {this.state.weightEnabled ? 'With weight' : 'Without weight'}
+          </Text>
+        </TouchableOpacity>
       </View>
-    )
-  }
+    </View>
+    );
 }
-
 
 let styles = StyleSheet.create({
   container: {
