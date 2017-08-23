8795e29d6ff0373438d534a16769a32e3ef9be0c
Ismail Sener <i_sener@hotmail.com.tr>
2017-08-23 16:17:30 +0300

    Initial Commit
    Android native ground overlays support implemented. OverlayManager has
    support also to transparency, but it's closed now.

--- lib/android/src/main/java/com/airbnb/android/react/maps/MapsPackage.java
+++ lib/android/src/main/java/com/airbnb/android/react/maps/MapsPackage.java
@@ -39,6 +39,7 @@ public class MapsPackage implements ReactPackage {
     AirMapManager mapManager = new AirMapManager(reactContext);
     AirMapLiteManager mapLiteManager = new AirMapLiteManager(reactContext);
     AirMapUrlTileManager tileManager = new AirMapUrlTileManager(reactContext);
+    AirMapOverlayManager overlayManager = new AirMapOverlayManager(reactContext);
 
     return Arrays.<ViewManager>asList(
         calloutManager,
@@ -48,6 +49,7 @@ public class MapsPackage implements ReactPackage {
         circleManager,
         mapManager,
         mapLiteManager,
-        tileManager);
+        tileManager,
+        overlayManager);
   }
 }
