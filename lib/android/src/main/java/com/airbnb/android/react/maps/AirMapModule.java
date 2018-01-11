0fcfa3a7088c3148b2ecd7bd365ab0ba7df6e3b4
André Vendramini <vendramini87@gmail.com>
2018-01-11 08:41:15 -0200

    Add missing import on AirMapModule.java (#1958)

--- lib/android/src/main/java/com/airbnb/android/react/maps/AirMapModule.java
+++ lib/android/src/main/java/com/airbnb/android/react/maps/AirMapModule.java
@@ -12,6 +12,7 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.ReadableMap;
+import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.bridge.WritableNativeMap;
 import com.facebook.react.uimanager.NativeViewHierarchyManager;
 import com.facebook.react.uimanager.UIBlock;
