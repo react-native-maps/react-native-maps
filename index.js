384756bb2a98a2f79aa4fe69b47ee502ec563d6d
Alexander Veligurov <aveligurov@gmail.com>
2018-02-12 18:12:22 -0300

    hotfix PROVIDER_GOOGLE

--- index.js
+++ index.js
@@ -10,5 +10,7 @@ export { default as Overlay } from './lib/components/MapOverlay.js';
 export { default as Callout } from './lib/components/MapCallout.js';
 export { default as AnimatedRegion } from './lib/components/AnimatedRegion.js';
 export { Animated, ProviderPropType, MAP_TYPES } from './lib/components/MapView.js';
+export const PROVIDER_GOOGLE = MapView.PROVIDER_GOOGLE;
+export const PROVIDER_DEFAULT = MapView.PROVIDER_DEFAULT;
 
 export default MapView;
