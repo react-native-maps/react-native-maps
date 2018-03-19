f245eae67bf2f51290027e50a8715e4db6d509eb
Harry Lachenmayer <harrylachenmayer@gmail.com>
2018-03-19 18:18:32 +0000

    Add an integration test target to the AirMapsExplorer iOS project.
    The test target currently does nothing, apart from importing `RCTTestRunner`.
    Following commits will create tests as described in
    https://facebook.github.io/react-native/docs/testing.html#ios
    
    This commit also makes the following changes to how Ruby gems are installed:
    
    1. Remove the bundler "binstubs" which were previously in `examples/ios`.
    
        Binstubs exist so that Ruby "binaries" can be executed without using
        `bundle exec`. [See here for an explanation.](https://github.com/rbenv/rbenv/wiki/Understanding-binstubs#project-specific-binstubs)
    
        The only place bundles are used currently is in the `build:ios` script in
        `package.json`. This already uses `bundle exec`, so the binstubs were
        actually never used.
    
    2. Install the Rubygem bundles locally in `example/ios/bundles`.
    
        `bundler` previously installed the dependencies in the global gems directory.
        If you are using the default macOS Ruby installation, this requires `sudo`.
    
        Since we already install node_modules & pods within the repo, I believe it
        makes sense to also install these locally.
    
    3. Update the `Gemfile.lock`.
    
        The current `Gemfile.lock` specified an outdated version of Cocoapods.
        This throws a warning on the current example Podfile.
        Cocoapods is now updated to version 1.4.0.

--- example/ios/AirMapsExplorerTests/AirMapsExplorerTests.m
+++ example/ios/AirMapsExplorerTests/AirMapsExplorerTests.m
@@ -1,20 +1,14 @@
-/**
- * Copyright (c) 2015-present, Facebook, Inc.
- * All rights reserved.
- *
- * This source code is licensed under the BSD-style license found in the
- * LICENSE file in the root directory of this source tree. An additional grant
- * of patent rights can be found in the PATENTS file in the same directory.
- */
+//
+//  AirMapsExplorerTests.m
+//  AirMapsExplorerTests
+//
+//  Created by Harry Lachenmayer on 19/03/2018.
+//  Copyright © 2018 Facebook. All rights reserved.
+//
 
-#import <UIKit/UIKit.h>
 #import <XCTest/XCTest.h>
 
-#import <React/RCTLog.h>
-#import <React/RCTRootView.h>
-
-#define TIMEOUT_SECONDS 240
-#define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
+#import <RCTTestRunner.h>
 
 @interface AirMapsExplorerTests : XCTestCase
 
@@ -22,49 +16,29 @@
 
 @implementation AirMapsExplorerTests
 
-- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
-{
-  if (test(view)) {
-    return YES;
-  }
-  for (UIView *subview in [view subviews]) {
-    if ([self findSubviewInView:subview matching:test]) {
-      return YES;
-    }
-  }
-  return NO;
+RCTTestRunner* _runner;
+
+- (void)setUp {
+    [super setUp];
+    
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    // In UI tests it is usually best to stop immediately when a failure occurs.
+    self.continueAfterFailure = NO;
+    // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+    [[[XCUIApplication alloc] init] launch];
+    
+    // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
 }
 
-- (void)testRendersWelcomeScreen
-{
-  UIViewController *vc = [[[[UIApplication sharedApplication] delegate] window] rootViewController];
-  NSDate *date = [NSDate dateWithTimeIntervalSinceNow:TIMEOUT_SECONDS];
-  BOOL foundElement = NO;
-
-  __block NSString *redboxError = nil;
-  RCTSetLogFunction(^(RCTLogLevel level, RCTLogSource source, NSString *fileName, NSNumber *lineNumber, NSString *message) {
-    if (level >= RCTLogLevelError) {
-      redboxError = message;
-    }
-  });
-
-  while ([date timeIntervalSinceNow] > 0 && !foundElement && !redboxError) {
-    [[NSRunLoop mainRunLoop] runMode:NSDefaultRunLoopMode beforeDate:[NSDate dateWithTimeIntervalSinceNow:0.1]];
-    [[NSRunLoop mainRunLoop] runMode:NSRunLoopCommonModes beforeDate:[NSDate dateWithTimeIntervalSinceNow:0.1]];
-
-    foundElement = [self findSubviewInView:vc.view matching:^BOOL(UIView *view) {
-      if ([view.accessibilityLabel isEqualToString:TEXT_TO_LOOK_FOR]) {
-        return YES;
-      }
-      return NO;
-    }];
-  }
-
-  RCTSetLogFunction(RCTDefaultLogFunction);
-
-  XCTAssertNil(redboxError, @"RedBox error: %@", redboxError);
-  XCTAssertTrue(foundElement, @"Couldn't find element with text '%@' in %d seconds", TEXT_TO_LOOK_FOR, TIMEOUT_SECONDS);
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
 }
 
+- (void)testExample {
+    // Use recording to get started writing UI tests.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
 
 @end
