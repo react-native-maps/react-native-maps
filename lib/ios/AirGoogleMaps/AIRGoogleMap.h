10b557f64f8b67405c98a97d7752c147c118fe51
David Cako <dc@cako.io>
2018-04-30 12:24:14 -0600

    heatmap fixes

--- lib/ios/AirGoogleMaps/AIRGoogleMap.h
+++ lib/ios/AirGoogleMaps/AIRGoogleMap.h
@@ -42,6 +42,7 @@
 @property (nonatomic, strong) NSMutableArray *polygons;
 @property (nonatomic, strong) NSMutableArray *polylines;
 @property (nonatomic, strong) NSMutableArray *circles;
+@property (nonatomic, strong) NSMutableArray *heatmaps;
 @property (nonatomic, strong) NSMutableArray *tiles;
 @property (nonatomic, strong) NSMutableArray *overlays;
 
