bcd181b1f79dd4cc04ce11d5468fa7f9e2306f14
funkyfourier <johs@ringheimsauto.org>
2018-05-13 13:07:04 +0200

    Make tiles display at the same physical size regardless of pixel dens… (#2248)
    * Make tiles display at the same physical size regardless of pixel density on iOS devices
    
    * Fix for timestamp issue
    
    * Removed plus sign

--- lib/ios/AirGoogleMaps/AIRGoogleMap.m
+++ lib/ios/AirGoogleMaps/AIRGoogleMap.m
@@ -481,7 +481,7 @@ id regionAsJSON(MKCoordinateRegion region) {
                     @"latitude": @(location.coordinate.latitude),
                     @"longitude": @(location.coordinate.longitude),
                     @"altitude": @(location.altitude),
-                    @"timestamp": @(location.timestamp),
+                    @"timestamp": @(location.timestamp.timeIntervalSinceReferenceDate * 1000),
                     @"accuracy": @(location.horizontalAccuracy),
                     @"altitudeAccuracy": @(location.verticalAccuracy),
                     @"speed": @(location.speed),
