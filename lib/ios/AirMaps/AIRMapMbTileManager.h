6d6f90a22f2e937aaf37b6d50768a53e4095a4be
Christoph <clambio@posteo.de>
2018-07-19 11:32:36 +0200

    Added MBTiles support for iOS and Android (#2208)
    * Added MBTiles support for iOS and Android
    
    * Added changes regarding the comments of @h3ll0w0rld123 from here: https://github.com/react-community/react-native-maps/pull/2208#discussion_r182597094
    
    * Added whitespaces
    
    * Hotfix: Imported exceptions. Changed database.close()
    
    * Hotfix: Removed the finally statemend. Resulted in always returning null
    
    * Removed repetition of returns. Moved everything into finally statement instead
    
    * Throwing exceptions
    
    * Added MapView.MbTile to Readme.
    
    Inclduded the component in the Readme
    
    * Added example file
    
    * Included more information in Readme
    
    * Edited example file accodring to linter errors in Pull Request
    
    * Edited index.d.ts according to merge conflicts in Pull Request. Had to change a few lines
    
    * Edited example file according to linter errors in merge request
    
    * Edited example file according to linter errors in merge request. I am starting not to like Travis...
    
    * Edited example file according to linter errors in merge request. I am starting not to like Travis...

---
+++ lib/ios/AirMaps/AIRMapMbTileManager.h
@@ -0,0 +1,14 @@
+//
+//  AIRMapMbTileManager.h
+//  AirMaps
+//
+//  Created by Christoph Lambio on 27/03/2018.
+//  Based on AIRMapLocalTileManager.h
+//  Copyright (c) 2017 Christopher. All rights reserved.
+//
+
+#import <React/RCTViewManager.h>
+
+@interface AIRMapMbTileManager : RCTViewManager
+
+@end
