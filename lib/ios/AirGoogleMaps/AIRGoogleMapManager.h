496426c9d47640b9f76bfc2075c7e49975dada15
Whitney Young <wbyoung@users.noreply.github.com>
2018-09-07 06:36:06 -0700

    Enable custom configuration for iOS maps (#2396)
    This allows setting up the library to work with Google Maps on iOS via
    `react-native link` instead of only via CocoaPods.
    
    Because Google-Maps-iOS-Utils is not distributed as a binary (at this
    time), it disables functionality that depends on this library and raises
    an exception that points to the reason why there's a problem.
    
    Additionally:
    
    - A missing file has been added back into the AirMaps.xcodeproj project.

--- lib/ios/AirGoogleMaps/AIRGoogleMapManager.h
+++ lib/ios/AirGoogleMaps/AIRGoogleMapManager.h
@@ -5,8 +5,12 @@
 //  Created by Gil Birman on 9/1/16.
 //
 
+#ifdef HAVE_GOOGLE_MAPS
+
 #import <React/RCTViewManager.h>
 
 @interface AIRGoogleMapManager : RCTViewManager
 
 @end
+
+#endif
--- lib/ios/AirGoogleMaps/AIRGoogleMapManager.m
+++ lib/ios/AirGoogleMaps/AIRGoogleMapManager.m
@@ -5,6 +5,8 @@
 //  Created by Gil Birman on 9/1/16.
 //
 
+#ifdef HAVE_GOOGLE_MAPS
+
 
 #import "AIRGoogleMapManager.h"
 #import <React/RCTViewManager.h>
@@ -473,3 +475,5 @@ RCT_EXPORT_METHOD(setMapBoundaries:(nonnull NSNumber *)reactTag
     [googleMapView didTapPOIWithPlaceID:placeID name:name location:location];
 }
 @end
+
+#endif
