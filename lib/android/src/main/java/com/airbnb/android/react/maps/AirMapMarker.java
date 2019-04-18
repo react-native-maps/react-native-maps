d6f726e376c6bebce024786a2d0928cd1ca6e96b
Alter Code <muhrizkie88@gmail.com>
2019-04-18 20:15:52 +0700

    add support for loading base64 encoded image (#2392)

--- lib/android/src/main/java/com/airbnb/android/react/maps/AirMapMarker.java
+++ lib/android/src/main/java/com/airbnb/android/react/maps/AirMapMarker.java
@@ -353,7 +353,7 @@ public class AirMapMarker extends AirMapFeature {
       iconBitmapDescriptor = null;
       update(true);
     } else if (uri.startsWith("http://") || uri.startsWith("https://") ||
-        uri.startsWith("file://") || uri.startsWith("asset://")) {
+        uri.startsWith("file://") || uri.startsWith("asset://") || uri.startsWith("data:")) {
       ImageRequest imageRequest = ImageRequestBuilder
           .newBuilderWithSource(Uri.parse(uri))
           .build();
