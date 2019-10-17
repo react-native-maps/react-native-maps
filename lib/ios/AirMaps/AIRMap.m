8ce61a95a0fda3a4e0af2c1fdb68581b3e6da503
Tuan Luong <tuanluong.it@gmail.com>
2019-10-18 04:43:17 +0700

    only handle cacheEnable when app is active (#3104)

--- lib/ios/AirMaps/AIRMap.m
+++ lib/ios/AirMaps/AIRMap.m
@@ -526,6 +526,11 @@ const NSInteger AIRMapMaxZoomLevel = 20;
 }
 
 - (void)cacheViewIfNeeded {
+    // https://github.com/react-native-community/react-native-maps/issues/3100
+    // Do nothing if app is not active
+    if ([[UIApplication sharedApplication] applicationState] != UIApplicationStateActive) {
+        return;
+    }
     if (self.hasShownInitialLoading) {
         if (!self.cacheEnabled) {
             if (_cacheImageView != nil) {
