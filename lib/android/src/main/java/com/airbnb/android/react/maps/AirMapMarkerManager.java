27c0e60b6c007819ef99523fcd7f8b1704ecb5f5
李建章 <hebeilijianzhang@163.com>
2020-02-27 22:59:52 +0800

    android google map callout support <Image/> (#3035)
    * android google map callout support <Image/>
    
    * fix eslint error

--- lib/android/src/main/java/com/airbnb/android/react/maps/AirMapMarkerManager.java
+++ lib/android/src/main/java/com/airbnb/android/react/maps/AirMapMarkerManager.java
@@ -270,14 +270,18 @@ public class AirMapMarkerManager extends ViewGroupManager<AirMapMarker> {
     view.setTracksViewChanges(tracksViewChanges);
   }
 
+  @ReactProp(name = "tracksInfoWindowChanges", defaultBoolean = true)
+  public void setTracksInfoWindowChanges(AirMapMarker view, boolean trackChanges) {
+    view.setTracksInfoWindowChanges(trackChanges);
+  }
+
   @Override
   public void addView(AirMapMarker parent, View child, int index) {
-    // if an <Callout /> component is a child, then it is a callout view, NOT part of the
-    // marker.
+    // add callout to parent ,otherwise image can not show
+    super.addView(parent, child, index);
     if (child instanceof AirMapCallout) {
       parent.setCalloutView((AirMapCallout) child);
     } else {
-      super.addView(parent, child, index);
       parent.update(true);
     }
   }
