8541e37e6037d699e4cf2a82f1631241eb0692fb
Cristian Aldea <aldea.cristian@outlook.com>
2020-03-23 13:16:25 -0400

    Include missing type definitions and fix readme
    While working on a react-native typescript project for one of our classes, we noticed that some of the type definitions and documentation were either missing or wrong. This commit aims to fix these minor issues

--- index.d.ts
+++ index.d.ts
@@ -164,6 +164,21 @@ declare module "react-native-maps" {
     position: Point;
   };
 
+  export type IndoorBuilding = {
+    underground: boolean,
+    activeLevelIndex: number,
+    levels: Array<IndoorLevel>,
+  }
+
+  export type IndoorLevel = {
+    index: number,
+    name: string,
+    shortName: string,
+  }
+
+  export interface IndoorBuildingEvent 
+    extends NativeSyntheticEvent<{IndoorBuilding:IndoorBuilding}> {}
+
   /**
    * onKmlReady parameter
    */
@@ -241,6 +256,7 @@ declare module "react-native-maps" {
     onMarkerDragStart?: (event: MapEvent) => void;
     onMarkerDrag?: (event: MapEvent) => void;
     onMarkerDragEnd?: (event: MapEvent) => void;
+    onIndoorBuildingFocused?: (event: IndoorBuildingEvent) => void; 
 
     minZoomLevel?: number;
     maxZoomLevel?: number;
@@ -275,6 +291,7 @@ declare module "react-native-maps" {
     takeSnapshot(options?: SnapshotOptions): Promise<string>;
     pointForCoordinate(coordinate: LatLng): Promise<Point>;
     coordinateForPoint(point: Point): Promise<LatLng>;
+    setIndoorActiveLevelIndex(index:number): void;
   }
 
   export class MapViewAnimated extends MapView {}
