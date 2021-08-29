810c105b68905df7fd4887ed290b17630a842f75
FreakyCoder <WrathChaos@users.noreply.github.com>
2021-08-29 20:43:24 +0300

    GeoJSON onPress Crash Fix and Marker Customization Options (#3920)
    * new: markerChildren for more customization options and onPress crash bug fix
    
    * fix: Linter fix
    
    * fix: Linter fix
    
    * Update geojson types, docs. Add prop-types
    
    Co-authored-by: Christopher Dro <hello@blick-labs.com>

--- example/examples/Geojson.js
+++ example/examples/Geojson.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import MapView, { Geojson } from 'react-native-maps';
-
+import { StyleSheet, Text } from 'react-native';
 const myPlace = {
   type: 'FeatureCollection',
   features: [
@@ -16,7 +16,7 @@ const myPlace = {
 };
 
 const GeojsonMap = props => (
-  <MapView>
+  <MapView style={{...StyleSheet.absoluteFillObject}}>
     <Geojson geojson={myPlace} />
   </MapView>
 );
