eb9142488e96de309e41175a57be9b9d8baa4f0d
Christopher Dro <hello@blick-labs.com>
2021-09-28 08:33:27 -0700

    Fix lint

--- index.js
+++ index.js
@@ -14,9 +14,7 @@ export { default as UrlTile } from './lib/components/MapUrlTile.js';
 export { default as WMSTile } from './lib/components/MapWMSTile.js';
 export { default as LocalTile } from './lib/components/MapLocalTile.js';
 export { default as Callout } from './lib/components/MapCallout.js';
-export {
-  default as CalloutSubview,
-} from './lib/components/MapCalloutSubview.js';
+export { default as CalloutSubview } from './lib/components/MapCalloutSubview.js';
 export { default as AnimatedRegion } from './lib/components/AnimatedRegion.js';
 export { default as Geojson } from './lib/components/Geojson.js';
 
