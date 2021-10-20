4580a2543cd4349105a524ba862fd4d6ad999b13
Simon-TechForm <73996878+Simon-TechForm@users.noreply.github.com>
2021-10-20 20:45:49 +0200

    build: type MapLocalTile

--- src/components/MapLocalTile.tsx
+++ src/components/MapLocalTile.tsx
@@ -1,49 +1,27 @@
-import PropTypes from 'prop-types';
 import React from 'react';
-
-import { ViewPropTypes, View } from 'react-native';
 
 import decorateMapComponent, {
   USES_DEFAULT_IMPLEMENTATION,
   SUPPORTED,
+  AirComponent,
+  UIManagerCommand,
+  MapManagerCommand,
+  ProviderContext,
 } from './decorateMapComponent';
+import { ViewProps } from 'react-native';
 
-// if ViewPropTypes is not defined fall back to View.propType (to support RN < 0.44)
-const viewPropTypes = ViewPropTypes || View.propTypes;
+export class MapLocalTile extends React.Component<Props> {
+  // declaration only, as they are set through decorateMap
+  declare context: React.ContextType<typeof ProviderContext>;
+  getAirComponent!: () => AirComponent<NativeProps>;
+  getMapManagerCommand!: (name: string) => MapManagerCommand;
+  getUIManagerCommand!: (name: string) => UIManagerCommand;
 
-const propTypes = {
-  ...viewPropTypes,
-
-  /**
-   * The path template of the local tile source.
-   * The patterns {x} {y} {z} will be replaced at runtime,
-   * for example, /storage/emulated/0/tiles/{z}/{x}/{y}.png.
-   */
-  pathTemplate: PropTypes.string.isRequired,
-
-  /**
-   * The order in which this tile overlay is drawn with respect to other overlays. An overlay
-   * with a larger z-index is drawn over overlays with smaller z-indices. The order of overlays
-   * with the same z-index is arbitrary. The default zIndex is -1.
-   *
-   * @platform android
-   */
-  zIndex: PropTypes.number,
-
-  /**
-   * Size of tile images.
-   */
-  tileSize: PropTypes.number,
-};
-
-class MapLocalTile extends React.Component {
   render() {
     const AIRMapLocalTile = this.getAirComponent();
     return <AIRMapLocalTile {...this.props} />;
   }
 }
-
-MapLocalTile.propTypes = propTypes;
 
 export default decorateMapComponent(MapLocalTile, {
   componentType: 'LocalTile',
@@ -54,3 +32,26 @@ export default decorateMapComponent(MapLocalTile, {
     },
   },
 });
+
+type Props = ViewProps & {
+  // flipY?: boolean;
+  /**
+   * @platform iOS: Apple Maps only
+   * @platform Android: Supported
+   */
+  pathTemplate: string;
+
+  /**
+   * @platform iOS: Apple Maps only
+   * @platform Android: Supported
+   */
+  tileSize?: number;
+
+  /**
+   * @platform iOS: Not supported
+   * @platform Android: Supported
+   */
+  zIndex?: number;
+};
+
+type NativeProps = Props;
