26682016900b7c3982040c7fe18ed3e482d635e2
Simon-TechForm <73996878+Simon-TechForm@users.noreply.github.com>
2021-10-20 20:41:51 +0200

    build: type decorateMapComponent

--- src/components/decorateMapComponent.ts
+++ src/components/decorateMapComponent.ts
@@ -1,12 +1,32 @@
-import PropTypes from 'prop-types';
-import { requireNativeComponent, NativeModules, Platform } from 'react-native';
+import { createContext } from 'react';
+import {
+  requireNativeComponent,
+  NativeModules,
+  UIManager,
+  Platform,
+  HostComponent,
+} from 'react-native';
+import { Provider } from '../types';
+import { MapCallout } from './MapCallout';
+import { MapCalloutSubview } from './MapCalloutSubview';
+import { MapCircle } from './MapCircle';
+import { MapHeatmap } from './MapHeatmap';
+import { MapLocalTile } from './MapLocalTile';
+import { MapMarker } from './MapMarker';
+import { MapOverlay } from './MapOverlay';
+import { MapPolygon } from './MapPolygon';
+import { MapPolyline } from './MapPolyline';
+import { MapUrlTile } from './MapUrlTile';
+import { MapWMSTile } from './MapWMSTile';
 import { PROVIDER_DEFAULT, PROVIDER_GOOGLE } from './ProviderConstants';
 
 export const SUPPORTED = 'SUPPORTED';
 export const USES_DEFAULT_IMPLEMENTATION = 'USES_DEFAULT_IMPLEMENTATION';
 export const NOT_SUPPORTED = 'NOT_SUPPORTED';
 
-export function getAirMapName(provider) {
+export const ProviderContext = createContext<Provider>(undefined);
+
+export function getAirMapName(provider: Provider) {
   if (Platform.OS === 'android') {
     return 'AIRMap';
   }
@@ -16,93 +36,95 @@ export function getAirMapName(provider) {
   return 'AIRMap';
 }
 
-function getAirComponentName(provider, component) {
+function getAirComponentName(provider: Provider, component: ComponentType) {
   return `${getAirMapName(provider)}${component}`;
 }
 
-export const contextTypes = {
-  provider: PropTypes.string,
-};
-
-export const createNotSupportedComponent = (message) => () => {
+export const createNotSupportedComponent = (message: string) => () => {
   console.error(message);
   return null;
 };
 
-function getViewManagerConfig(viewManagerName) {
-  const UIManager = NativeModules.UIManager;
-  if (!UIManager.getViewManagerConfig) {
-    // RN < 0.58
-    return UIManager[viewManagerName];
-  }
-  // RN >= 0.58
-  return UIManager.getViewManagerConfig(viewManagerName);
-}
-
-export const googleMapIsInstalled = !!getViewManagerConfig(
+// Intellisenst says that this always return true
+// I'm pretty sure that @types/react-native is wrong on this
+// getViewManagerConfig can return null, thus this function is still valid
+// see: https://github.com/facebook/react-native/blob/49b3b31d8e706338dac4ced1b372424d7d1d133f/Libraries/ReactNative/PaperUIManager.js
+// TODO: Sanity check and submit pull request to @types/react-native
+export const googleMapIsInstalled = !!UIManager.getViewManagerConfig(
   getAirMapName(PROVIDER_GOOGLE)
 );
 
-export default function decorateMapComponent(
-  Component,
-  { componentType, providers }
-) {
-  const components = {};
+export default function decorateMapComponent<Type extends ComponentClass>(
+  Component: Type,
+  {
+    componentType,
+    providers,
+  }: { componentType: ComponentType; providers: Providers }
+): Type {
+  const components: {
+    [key: string]: AirComponent;
+  } = {};
 
   const getDefaultComponent = () =>
-    requireNativeComponent(getAirComponentName(null, componentType), Component);
+    requireNativeComponent(getAirComponentName(undefined, componentType));
 
-  Component.contextTypes = contextTypes;
+  Component.contextType = ProviderContext;
 
-  Component.prototype.getAirComponent = function getAirComponent() {
-    const provider = this.context.provider || PROVIDER_DEFAULT;
-    if (components[provider]) {
-      return components[provider];
-    }
+  Component.prototype.getAirComponent =
+    function getAirComponent(): AirComponent {
+      const provider = this.context;
+      const key = provider || 'default';
 
-    if (provider === PROVIDER_DEFAULT) {
-      components[PROVIDER_DEFAULT] = getDefaultComponent();
-      return components[PROVIDER_DEFAULT];
-    }
-
-    const providerInfo = providers[provider];
-    const platformSupport = providerInfo[Platform.OS];
-    const componentName = getAirComponentName(provider, componentType);
-    if (platformSupport === NOT_SUPPORTED) {
-      components[provider] = createNotSupportedComponent(
-        `react-native-maps: ${componentName} is not supported on ${Platform.OS}`
-      );
-    } else if (platformSupport === SUPPORTED) {
-      if (
-        provider !== PROVIDER_GOOGLE ||
-        (Platform.OS === 'ios' && googleMapIsInstalled)
-      ) {
-        components[provider] = requireNativeComponent(componentName, Component);
+      if (components[key]) {
+        return components[key];
       }
-    } else {
-      // (platformSupport === USES_DEFAULT_IMPLEMENTATION)
-      if (!components[PROVIDER_DEFAULT]) {
-        components[PROVIDER_DEFAULT] = getDefaultComponent();
+
+      if (provider === PROVIDER_DEFAULT) {
+        components[key] = getDefaultComponent();
+        return components[key];
       }
-      components[provider] = components[PROVIDER_DEFAULT];
-    }
 
-    return components[provider];
-  };
+      const providerInfo = providers[provider];
+      // quick fix. Previous code assumed android | ios
+      if (Platform.OS !== 'android' && Platform.OS !== 'ios') {
+        throw new Error(`react-native-maps doesn't support ${Platform.OS}`);
+      }
+      const platformSupport = providerInfo[Platform.OS];
+      const componentName = getAirComponentName(provider, componentType);
+      if (platformSupport === NOT_SUPPORTED) {
+        components[key] = createNotSupportedComponent(
+          `react-native-maps: ${componentName} is not supported on ${Platform.OS}`
+        );
+      } else if (platformSupport === SUPPORTED) {
+        if (
+          provider !== PROVIDER_GOOGLE ||
+          (Platform.OS === 'ios' && googleMapIsInstalled)
+        ) {
+          components[key] = requireNativeComponent(componentName);
+        }
+      } else {
+        // (platformSupport === USES_DEFAULT_IMPLEMENTATION)
+        if (!components.default) {
+          components.default = getDefaultComponent();
+        }
+        components[key] = components.default;
+      }
 
-  Component.prototype.getUIManagerCommand = function getUIManagerCommand(name) {
-    const componentName = getAirComponentName(
-      this.context.provider,
-      componentType
-    );
-    return getViewManagerConfig(componentName).Commands[name];
+      return components[key];
+    };
+
+  Component.prototype.getUIManagerCommand = function getUIManagerCommand(
+    name: string
+  ): UIManagerCommand {
+    const componentName = getAirComponentName(this.context, componentType);
+    return UIManager.getViewManagerConfig(componentName).Commands[name];
   };
 
   Component.prototype.getMapManagerCommand = function getMapManagerCommand(
-    name
-  ) {
+    name: string
+  ): MapManagerCommand {
     const airComponentName = `${getAirComponentName(
-      this.context.provider,
+      this.context,
       componentType
     )}Manager`;
     return NativeModules[airComponentName][name];
@@ -110,3 +132,44 @@ export default function decorateMapComponent(
 
   return Component;
 }
+
+type Providers = {
+  google: {
+    ios: 'SUPPORTED' | 'USES_DEFAULT_IMPLEMENTATION' | 'NOT_SUPPORTED';
+    android: 'SUPPORTED' | 'USES_DEFAULT_IMPLEMENTATION' | 'NOT_SUPPORTED';
+  };
+};
+
+type ComponentClass =
+  | typeof MapCallout
+  | typeof MapCalloutSubview
+  | typeof MapCircle
+  | typeof MapHeatmap
+  | typeof MapLocalTile
+  | typeof MapMarker
+  | typeof MapOverlay
+  | typeof MapPolygon
+  | typeof MapPolyline
+  | typeof MapUrlTile
+  | typeof MapWMSTile;
+
+type ComponentType =
+  | 'Callout'
+  | 'CalloutSubview'
+  | 'Circle'
+  | 'Heatmap'
+  | 'LocalTile'
+  | 'Marker'
+  | 'Overlay'
+  | 'Polygon'
+  | 'Polyline'
+  | 'UrlTile'
+  | 'WMSTile';
+
+//todo: narrow down any
+export type UIManagerCommand = any;
+export type MapManagerCommand = any;
+
+export type AirComponent<H = unknown> =
+  | HostComponent<H>
+  | ReturnType<typeof createNotSupportedComponent>;
