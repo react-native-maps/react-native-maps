d3f4dd36489affb9cacf45e0748ddc7f674b2cb8
Jason White <jwhite@playboomsports.com>
2021-11-16 17:32:18 -0500

    CHANGED: AnimatedRegion.test.js to also test values

--- __tests__/AnimatedRegion.test.js
+++ __tests__/AnimatedRegion.test.js
@@ -1,24 +1,35 @@
 import { Animated } from 'react-native';
 import AnimatedRegion from '../lib/components/AnimatedRegion';
 
+const VALUES = {
+  latitude: 5,
+  longitude: 5,
+  latitudeDelta: 0,
+  longitudeDelta: 0
+}
+
 describe('AnimatedRegion', () => {
   it('converts numbers to instances of Animated.Value', () => {
-    const animatedRegion = new AnimatedRegion({
-      latitude: 5,
-      longitude: 5,
-    });
+    const animatedRegion = new AnimatedRegion(VALUES);
     const { latitude, longitude, latitudeDelta, longitudeDelta } = animatedRegion;
 
     expect(latitude instanceof Animated.Value).toBe(true);
     expect(longitude instanceof Animated.Value).toBe(true);
     expect(latitudeDelta instanceof Animated.Value).toBe(true);
     expect(longitudeDelta instanceof Animated.Value).toBe(true);
+
+    const values = animatedRegion.__getValue();
+
+    expect(values.latitude).toEqual(VALUES.latitude);
+    expect(values.longitude).toEqual(VALUES.longitude);
+    expect(values.longitudeDelta).toEqual(VALUES.longitudeDelta);
+    expect(values.latitudeDelta).toEqual(VALUES.latitudeDelta);
   });
 
   it('uses Animated.Value instances', () => {
     const animatedRegion = new AnimatedRegion({
-      latitude: new Animated.Value(5),
-      longitude: new Animated.Value(5),
+      latitude: new Animated.Value(VALUES.latitude),
+      longitude: new Animated.Value(VALUES.longitude),
     });
     const { latitude, longitude, latitudeDelta, longitudeDelta } = animatedRegion;
 
@@ -26,6 +37,13 @@ describe('AnimatedRegion', () => {
     expect(longitude instanceof Animated.Value).toBe(true);
     expect(latitudeDelta instanceof Animated.Value).toBe(true);
     expect(longitudeDelta instanceof Animated.Value).toBe(true);
+
+    const values = animatedRegion.__getValue();
+
+    expect(values.latitude).toEqual(VALUES.latitude);
+    expect(values.longitude).toEqual(VALUES.longitude);
+    expect(values.longitudeDelta).toEqual(VALUES.longitudeDelta);
+    expect(values.latitudeDelta).toEqual(VALUES.latitudeDelta);
   });
 
   it('uses defaults converted to Animated.Value instances when none are supplied', () => {
@@ -36,5 +54,12 @@ describe('AnimatedRegion', () => {
     expect(longitude instanceof Animated.Value).toBe(true);
     expect(latitudeDelta instanceof Animated.Value).toBe(true);
     expect(longitudeDelta instanceof Animated.Value).toBe(true);
+
+    const values = animatedRegion.__getValue();
+
+    expect(values.latitude).toEqual(0);
+    expect(values.longitude).toEqual(0);
+    expect(values.longitudeDelta).toEqual(VALUES.longitudeDelta);
+    expect(values.latitudeDelta).toEqual(VALUES.latitudeDelta);
   })
 });
