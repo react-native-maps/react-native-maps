64f418121638a0d9ef2474712a8a0ec30cdca48a
Jason White <jwhite@playboomsports.com>
2021-11-17 16:48:56 -0500

    Feature fix `AnimatedRegion` `getAnimatedValue` Issue #4010 (#4011)
    * CHANGED: getAnimatedValue to differentiate more cleanly between provided values, and fix a regression where we failed to convert a valid valueIn to Animated.Value
    
    * ADDED: dev testing setup (prelim) and AnimatedRegion.test.js to confirm both values in and defaults are ultimately converted to Animated.Value instances
    
    * CHANGED: AnimatedRegion.test.js to also test values
    
    * REMOVED: unnecessary deps
    
    * FIXED: erroneously removed devDeps
    
    * FIXED: formatting wonkiness in AnimatedRegion.test.js
    
    * FIXED: package.json versions (trying to resolve build errors)
    
    * FIXED: build prettier errors
    
    * FIXED: build prettier errors
    
    * FIXED: build prettier errors

---
+++ __tests__/AnimatedRegion.test.js
@@ -0,0 +1,62 @@
+import { Animated } from 'react-native';
+import AnimatedRegion from '../lib/components/AnimatedRegion';
+
+const VALUES = {
+  latitude: 5,
+  longitude: 5,
+  latitudeDelta: 0,
+  longitudeDelta: 0,
+};
+
+describe('AnimatedRegion', () => {
+  it('converts numbers to instances of Animated.Value', () => {
+    const animatedRegion = new AnimatedRegion(VALUES);
+
+    expect(animatedRegion.latitude instanceof Animated.Value).toBe(true);
+    expect(animatedRegion.longitude instanceof Animated.Value).toBe(true);
+    expect(animatedRegion.latitudeDelta instanceof Animated.Value).toBe(true);
+    expect(animatedRegion.longitudeDelta instanceof Animated.Value).toBe(true);
+
+    const values = animatedRegion.__getValue();
+
+    expect(values.latitude).toEqual(VALUES.latitude);
+    expect(values.longitude).toEqual(VALUES.longitude);
+    expect(values.longitudeDelta).toEqual(VALUES.longitudeDelta);
+    expect(values.latitudeDelta).toEqual(VALUES.latitudeDelta);
+  });
+
+  it('uses Animated.Value instances', () => {
+    const animatedRegion = new AnimatedRegion({
+      latitude: new Animated.Value(VALUES.latitude),
+      longitude: new Animated.Value(VALUES.longitude),
+    });
+
+    expect(animatedRegion.latitude instanceof Animated.Value).toBe(true);
+    expect(animatedRegion.longitude instanceof Animated.Value).toBe(true);
+    expect(animatedRegion.latitudeDelta instanceof Animated.Value).toBe(true);
+    expect(animatedRegion.longitudeDelta instanceof Animated.Value).toBe(true);
+
+    const values = animatedRegion.__getValue();
+
+    expect(values.latitude).toEqual(VALUES.latitude);
+    expect(values.longitude).toEqual(VALUES.longitude);
+    expect(values.longitudeDelta).toEqual(VALUES.longitudeDelta);
+    expect(values.latitudeDelta).toEqual(VALUES.latitudeDelta);
+  });
+
+  it('uses defaults converted to Animated.Value instances when none are supplied', () => {
+    const animatedRegion = new AnimatedRegion({});
+
+    expect(animatedRegion.latitude instanceof Animated.Value).toBe(true);
+    expect(animatedRegion.longitude instanceof Animated.Value).toBe(true);
+    expect(animatedRegion.latitudeDelta instanceof Animated.Value).toBe(true);
+    expect(animatedRegion.longitudeDelta instanceof Animated.Value).toBe(true);
+
+    const values = animatedRegion.__getValue();
+
+    expect(values.latitude).toEqual(0);
+    expect(values.longitude).toEqual(0);
+    expect(values.longitudeDelta).toEqual(VALUES.longitudeDelta);
+    expect(values.latitudeDelta).toEqual(VALUES.latitudeDelta);
+  });
+});
