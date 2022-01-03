af8ef6fd2b511d33d38c8fce038ef55c66cb4ccd
JimTeva <10006390+JimTeva@users.noreply.github.com>
2022-01-03 08:56:53 -1000

    Update index.d.ts (#4039)
    l.83 & l.100: useNativeDriver is required in type 'AnimationConfig'.
    
    l.103-->107: Incorrect use of class Animated instead of RNAnimated.

--- index.d.ts
+++ index.d.ts
@@ -80,7 +80,7 @@ declare module 'react-native-maps' {
     easing?: (value: number) => number;
     duration?: number;
     delay?: number;
-    useNativeDriver?: boolean;
+    useNativeDriver: boolean;
   }
 
   interface AnimatedRegionSpringConfig
@@ -97,14 +97,14 @@ declare module 'react-native-maps' {
     stiffness?: number;
     mass?: number;
     damping?: number;
-    useNativeDriver?: boolean;
+    useNativeDriver: boolean;
   }
 
-  export class AnimatedRegion extends Animated.AnimatedWithChildren {
-    latitude: Animated.Value;
-    longitude: Animated.Value;
-    latitudeDelta: Animated.Value;
-    longitudeDelta: Animated.Value;
+  export class AnimatedRegion extends RNAnimated.AnimatedWithChildren {
+    latitude: RNAnimated.Value;
+    longitude: RNAnimated.Value;
+    latitudeDelta: RNAnimated.Value;
+    longitudeDelta: RNAnimated.Value;
 
     constructor(region?: Region);
 
