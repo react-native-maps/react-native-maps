4592499c153804de5dca5a17c2015338baf2767b
Simon-TechForm <73996878+Simon-TechForm@users.noreply.github.com>
2022-04-11 14:46:51 +0200

    fix(android): remove use of lambda expression
    Resolves #4160

--- android/src/main/java/com/airbnb/android/react/maps/AirMapModule.java
+++ android/src/main/java/com/airbnb/android/react/maps/AirMapModule.java
@@ -10,6 +10,8 @@ import android.util.Base64;
 import android.util.DisplayMetrics;
 import android.util.Log;
 
+import androidx.annotation.NonNull;
+
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
@@ -23,6 +25,7 @@ import com.facebook.react.uimanager.UIBlock;
 import com.facebook.react.uimanager.UIManagerModule;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapsSdkInitializedCallback;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 
@@ -365,9 +368,12 @@ public class AirMapModule extends ReactContextBaseJavaModule {
       @Override
       public void execute(NativeViewHierarchyManager nvhm)
       {
-        MapsInitializer.initialize(context, MapsInitializer.Renderer.LATEST, (MapsInitializer.Renderer renderer) -> {
-          Log.d("AirMapRenderer", renderer.toString());
-          promise.resolve(renderer.toString());
+        MapsInitializer.initialize(context, MapsInitializer.Renderer.LATEST, new OnMapsSdkInitializedCallback() {
+          @Override
+          public void onMapsSdkInitialized(@NonNull MapsInitializer.Renderer renderer) {
+            Log.d("AirMapRenderer", renderer.toString());
+            promise.resolve(renderer.toString());
+          }
         });
       }
     });
