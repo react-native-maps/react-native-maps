4c52dc163ebd8885777b619609b9ccf732df1157
Filip Horvei <filip@horvei.com>
2022-07-07 14:21:06 +0200

    fix(ios): wrong coordinates on pressing polyline (#4304)
    BREAKING CHANGE: polyline onPress returns tapLocation instead of the first point in the polyline

--- ios/AirMaps/AIRMapManager.m
+++ ios/AirMaps/AIRMapManager.m
@@ -671,12 +671,11 @@ RCT_EXPORT_METHOD(getAddressFromCoordinates:(nonnull NSNumber *)reactTag
     }
 
     if (nearestDistance <= maxMeters) {
-        AIRMapCoordinate *firstCoord = nearestPolyline.coordinates.firstObject;
         id event = @{
                    @"action": @"polyline-press",
                    @"coordinate": @{
-                       @"latitude": @(firstCoord.coordinate.latitude),
-                       @"longitude": @(firstCoord.coordinate.longitude)
+                       @"latitude": @(tapCoordinate.latitude),
+                       @"longitude": @(tapCoordinate.longitude)
                    }
                    };
         nearestPolyline.onPress(event);
