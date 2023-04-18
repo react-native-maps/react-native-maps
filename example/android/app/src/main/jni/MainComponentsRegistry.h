079156f07cc8318cda6d90e7adf9138b7a9c4645
Simon-TechForm <73996878+Simon-TechForm@users.noreply.github.com>
2023-04-18 21:50:22 +0200

    chore: rename example app

--- example/android/app/src/main/jni/MainComponentsRegistry.h
+++ example/android/app/src/main/jni/MainComponentsRegistry.h
@@ -13,7 +13,7 @@ class MainComponentsRegistry
  public:
   // Adapt it to the package you used for your Java class.
   constexpr static auto kJavaDescriptor =
-      "Lcom/example/newarchitecture/components/MainComponentsRegistry;";
+      "Lcom/rnmshowcase/newarchitecture/components/MainComponentsRegistry;";
 
   static void registerNatives();
 
