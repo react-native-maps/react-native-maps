26afda7d51f3c17b35df683e6d1a515c02c33ad7
Simon-TechForm <73996878+Simon-TechForm@users.noreply.github.com>
2023-04-18 21:50:22 +0200

    chore: rename example app

--- example/android/app/src/main/java/com/rnmshowcase/newarchitecture/components/MainComponentsRegistry.java
+++ example/android/app/src/main/java/com/rnmshowcase/newarchitecture/components/MainComponentsRegistry.java
@@ -1,4 +1,4 @@
-package com.example.newarchitecture.components;
+package com.rnmshowcase.newarchitecture.components;
 
 import com.facebook.jni.HybridData;
 import com.facebook.proguard.annotations.DoNotStrip;
