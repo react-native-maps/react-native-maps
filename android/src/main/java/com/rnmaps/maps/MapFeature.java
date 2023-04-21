93ab295bd5297492cae9cd7990950bc50c1a05c7
Simon-TechForm <73996878+Simon-TechForm@users.noreply.github.com>
2023-04-21 23:34:21 +0200

    feat(android): bump android-maps-utils to 3.4.0

--- android/src/main/java/com/rnmaps/maps/MapFeature.java
+++ android/src/main/java/com/rnmaps/maps/MapFeature.java
@@ -3,16 +3,15 @@ package com.rnmaps.maps;
 import android.content.Context;
 
 import com.facebook.react.views.view.ReactViewGroup;
-import com.google.android.gms.maps.GoogleMap;
 
 public abstract class MapFeature extends ReactViewGroup {
   public MapFeature(Context context) {
     super(context);
   }
 
-  public abstract void addToMap(GoogleMap map);
+  public abstract void addToMap(Object mapOrCollection);
 
-  public abstract void removeFromMap(GoogleMap map);
+  public abstract void removeFromMap(Object mapOrCollection);
 
   public abstract Object getFeature();
 }
