0856cac1e65bbf676030cb9915c2813a98d713ba
salah ghanim <salah@getaway.pro>
2024-04-01 11:58:28 +0200

    chore(update-RN): fix compilation for android   - use BuildConfig that's auto gen

--- android/src/main/java/com/rnmaps/maps/MapView.java
+++ android/src/main/java/com/rnmaps/maps/MapView.java
@@ -31,7 +31,6 @@ import com.facebook.react.bridge.WritableNativeMap;
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.uimanager.ThemedReactContext;
 import com.facebook.react.uimanager.UIManagerHelper;
-import com.facebook.react.uimanager.UIManagerModule;
 import com.facebook.react.uimanager.common.UIManagerType;
 import com.facebook.react.uimanager.events.EventDispatcher;
 import com.google.android.gms.maps.CameraUpdate;
