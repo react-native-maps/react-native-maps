1310985e6e2c0d5b915d7364cf52a99cca43ec8f
salah ghanim <salah@getaway.pro>
2024-04-13 16:56:45 +0200

    feat: add support for new React Native architecture - example for android is working as well

--- example/react-native.config.js
+++ example/react-native.config.js
@@ -8,6 +8,22 @@ module.exports = {
     },
   },
   project: {
+    android: {
+      unstable_reactLegacyComponentNames: [
+        'AIRMap',
+        'AIRMapCallout',
+        'AIRMapCalloutSubview',
+        'AIRMapCircle',
+        'AIRMapHeatmap',
+        'AIRMapLocalTile',
+        'AIRMapMarker',
+        'AIRMapOverlay',
+        'AIRMapPolygon',
+        'AIRMapPolyline',
+        'AIRMapUrlTile',
+        'AIRMapWMSTile',
+      ],
+    },
     ios: {
       unstable_reactLegacyComponentNames: [
         'AIRMap',
