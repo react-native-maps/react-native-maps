942d58e260ac03ea49fb9e81b896a8a742dbf838
Mateusz Bętka <37906388+mateki0@users.noreply.github.com>
2024-05-06 10:43:44 +0200

    Chore: example app w/react-native to 0.74 (#5040)

--- example/android/app/src/main/java/com/rnmshowcase/MainApplication.kt
+++ example/android/app/src/main/java/com/rnmshowcase/MainApplication.kt
@@ -9,8 +9,8 @@ import com.facebook.react.ReactPackage
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
-import com.facebook.react.flipper.ReactNativeFlipper
 import com.facebook.soloader.SoLoader
+
 class MainApplication : Application(), ReactApplication {
     override val reactNativeHost: ReactNativeHost =
         object : DefaultReactNativeHost(this) {
@@ -25,7 +25,7 @@ class MainApplication : Application(), ReactApplication {
             override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
         }
     override val reactHost: ReactHost
-        get() = getDefaultReactHost(this.applicationContext, reactNativeHost)
+        get() = getDefaultReactHost(applicationContext, reactNativeHost)
     override fun onCreate() {
         super.onCreate()
         SoLoader.init(this, false)
@@ -33,6 +33,5 @@ class MainApplication : Application(), ReactApplication {
             // If you opted-in for the New Architecture, we load the native entry point for this app.
             load()
         }
-        ReactNativeFlipper.initializeFlipper(this, reactNativeHost.reactInstanceManager)
     }
 }
