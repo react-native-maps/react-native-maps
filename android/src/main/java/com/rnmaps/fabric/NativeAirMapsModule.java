423633bae4d0462b864c21f1a8e4484225d9724e
salah ghanim <salah@getaway.pro>
2025-02-08 20:03:58 +0100

    feat(android): support for Fabric  - getCamera

--- android/src/main/java/com/rnmaps/fabric/NativeAirMapsModule.java
+++ android/src/main/java/com/rnmaps/fabric/NativeAirMapsModule.java
@@ -1,9 +1,15 @@
 package com.rnmaps.fabric;
 
 import com.facebook.fbreact.specs.NativeAirMapsModuleSpec;
+import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReadableMap;
+import com.facebook.react.bridge.UIManager;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.uimanager.UIManagerHelper;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.rnmaps.maps.MapView;
 
 public class NativeAirMapsModule extends NativeAirMapsModuleSpec {
     public NativeAirMapsModule(ReactApplicationContext reactContext) {
@@ -17,7 +23,23 @@ public class NativeAirMapsModule extends NativeAirMapsModuleSpec {
 
     @Override
     public void getCamera(double tag, Promise promise) {
-
+            UIManager uiManager = UIManagerHelper.getUIManagerForReactTag(getReactApplicationContext(), (int) tag);
+            getReactApplicationContext().runOnUiQueueThread(new Runnable() {
+                @Override
+                public void run() {
+                    MapView view = (MapView) uiManager.resolveView((int) tag);
+                    CameraPosition position = view.map.getCameraPosition();
+                    WritableMap map = Arguments.createMap();
+                    WritableMap center = Arguments.createMap();
+                    center.putDouble("latitude", position.target.latitude);
+                    center.putDouble("longitude", position.target.longitude);
+                    map.putMap("center", center);
+                    map.putDouble("heading", position.bearing);
+                    map.putDouble("pitch", position.tilt);
+                    map.putDouble("zoom", position.zoom);
+                    promise.resolve(map);
+                }
+            });
     }
 
     @Override
