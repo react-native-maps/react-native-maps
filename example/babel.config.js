f52bc9f5e68c5f84f1bef3cadb178e85d182163f
pinpong <o.nitzschke@gmail.com>
2025-02-28 06:51:45 +0100

    Revert "revert: allowDeclareFields check"
    This reverts commit 93f42f3395e91e6ad94c5f522c5687a196c33ef4.

--- example/babel.config.js
+++ example/babel.config.js
@@ -7,15 +7,6 @@ const root = path.resolve(__dirname, '..');
 module.exports = getConfig(
   {
     presets: ['module:@react-native/babel-preset'],
-    plugins: [
-      [
-        '@babel/plugin-transform-typescript',
-        {
-          /// TODO: remove this override
-          allowDeclareFields: true,
-        },
-      ],
-    ],
   },
   { root, pkg }
 );
