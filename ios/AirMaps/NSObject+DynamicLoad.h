a4c3ca6f4463ac62c40bd31510ba8775bd527674
salah ghanim <salah@getaway.pro>
2025-03-16 19:34:23 +0100

    feat(android-ios): support for Fabric   - a simpler dynamic way to inject RNMapsGooglePolygonView   - and RNMapsGoogleMapView empty classes if non is available

---
+++ ios/AirMaps/NSObject+DynamicLoad.h
@@ -0,0 +1,16 @@
+//
+//  NSObject+DynamicLoad.h
+//  react-native-maps
+//
+//  Created by Salah Ghanim on 16.03.25.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NSObject (DynamicLoad)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ios/AirMaps/NSObject+DynamicLoad.m
@@ -0,0 +1,33 @@
+//
+//  NSObject+DynamicLoad.m
+//  react-native-maps
+//
+//  Created by Salah Ghanim on 16.03.25.
+//
+#import <Foundation/Foundation.h>
+#import <objc/runtime.h>
+#import "NSObject+DynamicLoad.h"
+
+@implementation NSObject (DynamicLoad)
++ (void)load {
+    [self ensureClassExists:"RNMapsGoogleMapView"];
+    [self ensureClassExists:"RNMapsGooglePolygonView"];
+}
+
++ (void)ensureClassExists:(const char *)className {
+    // Check if the class exists
+    Class existingClass = objc_getClass(className);
+    if (!existingClass) {
+        // If the class does not exist, create a new class that is a subclass of NSObject
+        Class newClass = objc_allocateClassPair([NSObject class], className, 0);
+
+        // Register the new class with the runtime
+        objc_registerClassPair(newClass);
+
+        NSLog(@"Class %s created successfully.", className);
+    } else {
+        NSLog(@"Class %s already exists.", className);
+    }
+}
+
+@end
