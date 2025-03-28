e87884e5345f7019fa194dd6dfe4a94292e3867f
salah ghanim <salah@getaway.pro>
2025-03-29 00:43:40 +0100

    feat(android): support for Fabric   - include react-native.config.js in npm pack   - use path that works with external apps

--- react-native.config.js
+++ react-native.config.js
@@ -9,7 +9,7 @@ module.exports = {
   dependency: {
     platforms: {
       android: {
-        cmakeListsPath: '../android/src/main/jni/CMakeLists.txt',
+        cmakeListsPath: 'src/main/jni/CMakeLists.txt',
       },
     },
   },
