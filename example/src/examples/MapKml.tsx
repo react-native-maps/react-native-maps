0bbb53d16d89891370cdd3e3de4ff73d31ea1e0b
Alexis CHAPPRON <bullrot-10@hotmail.fr>
2025-04-30 12:07:51 +0200

    more accurate dispatch_group_leave

--- example/src/examples/MapKml.tsx
+++ example/src/examples/MapKml.tsx
@@ -57,6 +57,7 @@ const MapKml = ({ provider }: MapKmlProps) => {
   };
 
   const handleOnKmlReady = () => {
+    console.log("onKmlReady called");
     mapRef.current?.animateToRegion({
       latitude: LATITUDE,
       longitude: LONGITUDE,
