1b62fdc000ad3aaf840d1c82ac743ce8915cc2e9
salah ghanim <salah@getaway.pro>
2025-05-02 13:46:21 +0200

    chore: add support for release from yarn workspace    - attempt to use exec directly instead of the npm plugin

---
+++ packages/lib/release.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  branches: ['main'],
+  tagFormat: 'v${version}',
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    '@semantic-release/git',
+    [
+      '@semantic-release/exec',
+      {
+        prepareCmd:
+          'yarn workspace react-native-maps version ${nextRelease.version} --no-git-tag-version',
+        publishCmd: 'npm publish --workspace packages/lib --access public',
+      },
+    ],
+    '@semantic-release/github',
+  ],
+};
